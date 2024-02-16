8934095655c252f0cc0dba55509f449a28a27dc2
James Noeckel <jamesn8@cs.washington.edu>
2024-02-15 22:37:47 -0800

    add setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages, Extension
+
+setup(name='mask2former',
+    version='0.0.1',
+    description='forming masks',
+    author='Mark Zuckerberg',
+    author_email='mark@meta.com',
+    license='MIT',
+    packages=find_packages()
+)
